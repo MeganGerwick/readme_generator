eb314250c54a0db36dff7e8b02689baff2f8843c
Louisa May <mgerwick@gmail.com>
2020-11-13 13:50:15 -0600

    added question prompts

--- index.js
+++ index.js
@@ -2,19 +2,69 @@ const inquirer = require('inquirer')
 const fs = require('fs');
 const util = require('util');
 
-// array of questions for user
-const questions = [
+const writeFileAsync = util.promisfy(fs.writeFile);
 
-];
+// array of questions for user
+const questions = (data) =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Username',
+            message: 'What is your github username?',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Please write a short description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'License',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'Install Dependencies',
+            message: 'What command should be used to install dependencies?'['npm i'],
+        },
+        {
+            type: 'input',
+            name: 'User Info',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'Contribute',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
+
+
+    ]);
 
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = (data) => {
+    '#' + ${ data.title }
+    ;
 }
 
 // function to initialize program
-function init() {
-
+function questions() {
+.then((data) => writeFileAsync('README.md', writeToFile(data)))
+        .then(() => console.log('Successfully wrote ReadMe'))
+        .catch((err) => console.error(err));
 }
 
 // function call to initialize program
-init();
+questions()
+

5133385c5947250f0b0c1cbc34f1fe85afa00236
Louisa May <mgerwick@gmail.com>
2020-11-24 21:44:23 -0600

    fixed indent issue

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# titole
+# woeijt
+## Description
+we3orwu
 
+### Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[Contributing](#contributing)
+[Questions](#questions)
+
+## Installation
+To install the necessary dependencies for this project, use the command weoijw.
+
+## Usage
+weoij
+
+## Contributing
+weoi
+
+## Questions
+If you have questions about this repository, contact woeij at https://github.com/woeij
+or email at woejirw.
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,28 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.Title}
-    ## Description
-    ${data.Description}
+## Description
+${data.Description}
 
-    ## Table of Contents
-    [Installation](https://github.com/${data.Username}/${data.Title}#installation)
-    [Usage](https://github.com/${data.Username}/${data.Title}#usage)
-    [Contributing](https://github.com/${data.Username}/${data.Title}#contributing)
-    [Questions](https://github.com/${data.Username}/${data.Title}#questions)
+### Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[Contributing](#contributing)
+[Questions](#questions)
     
-    ## Installation
-    To install the necessary dependencies for this project, use the command ${data.Install}.
-    
-    ## Usage
-    ${data.Usage}
+## Installation
+To install the necessary dependencies for this project, use the command ${data.Install}.
 
-    ## Contributing
-    ${data.Contribute}
+## Usage
+${data.Usage}
 
-    ## Questions
-    If you have questions about this repository, contact ${data.Username} at https://github.com/${data.Username}
-    or email at ${data.Email}.
+## Contributing
+${data.Contribute}
+
+## Questions
+If you have questions about this repository, contact ${data.Username} at https://github.com/${data.Username}
+or email at ${data.Email}.
 `;
-}
+};
 
 module.exports = generateMarkdown;
